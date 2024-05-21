3ae45012167e06c3a1201d3ec916677ec024efaf
5llO0o <86219105+5llO0o@users.noreply.github.com>
2024-05-22 03:06:10 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('nextButton').addEventListener('click', function() {
+    document.getElementById('screen1').style.display = 'none';
+    document.getElementById('screen2').style.display = 'block';
+});
