6a598dd61de777dc1acb886de29f24687499af01
5llO0o <jungyuggal8736@naver.com>
2024-06-16 22:48:25 +0900

    update

--- js/gallery.js
+++ js/gallery.js
@@ -1,21 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
-	const slider = document.querySelector('.slider');
-	const gallery = document.querySelector('.gallery');
-	const images = gallery.getElementsByTagName('li');
-	const totalImages = images.length;
-	let currentIndex = 0;
-	const speed = 8000; // 5 seconds interval
-	
-	function showNextImage() {
-		const sliderWidth = slider.clientWidth;
-		currentIndex = (currentIndex + 1) % totalImages;
-		gallery.style.transform = `translateX(-${currentIndex * sliderWidth}px)`;
-	}
-	
-	setInterval(showNextImage, speed);
-	window.addEventListener('resize', () => {
-		const sliderWidth = slider.clientWidth;
-		gallery.style.transform = `translateX(-${currentIndex * sliderWidth}px)`;
-	});
-
+  const galleryList = document.getElementById('gallery');
+  
+  // Function to rotate items in the gallery
+  function rotateGallery() {
+    const firstItem = galleryList.firstElementChild;
+    galleryList.removeChild(firstItem);
+    galleryList.appendChild(firstItem);
+  }
+  
+  // Rotate gallery every 3 seconds (adjust interval as needed)
+  setInterval(rotateGallery, 3000);
 });
